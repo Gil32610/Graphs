ae40e0fecbbd1d5d36407867058d3c3c17cead9f
GIL32610 <gil.00000032610@unicap.br>
2023-10-01 21:52:06 -0300

    Status and BFS

--- src/GraphWithList/GraphNodeNeighbourList.java
+++ src/GraphWithList/GraphNodeNeighbourList.java
@@ -3,15 +3,21 @@ package GraphWithList;
 import java.util.ArrayList;
 
 public class GraphNodeNeighbourList {
+    public static final int BRANCO = 0;
+    public static final int CINZA = 1;
+
+    public static final int PRETO = 2;
 
     private Character name;
     private int index;
     private ArrayList<GraphNodeNeighbourList> neighbours;
+    private int status;
 
     public GraphNodeNeighbourList(Character name, int index) {
         this.neighbours = new ArrayList<>();
         this.name = name;
         this.index = index;
+        this.status = BRANCO;
     }
 
     public Character getName() {
@@ -32,5 +38,13 @@ public class GraphNodeNeighbourList {
                 "{name=" + name +
                 ", index=" + index +
                 '}';
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
     }
 }
--- src/GraphWithList/GraphWithAdjList.java
+++ src/GraphWithList/GraphWithAdjList.java
@@ -1,17 +1,28 @@
 package GraphWithList;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class GraphWithAdjList {
+
+    private int[] ante;
+    private int[]d;
+    private int[]cor;
 private ArrayList<GraphNodeNeighbourList> adjacencyList;
 
     public GraphWithAdjList(ArrayList<GraphNodeNeighbourList> adjacencyList) {
         this.adjacencyList = adjacencyList;
+        this.d = new int[adjacencyList.size()];
+        this.cor = new int[adjacencyList.size()];
+        this.ante = new int[adjacencyList.size()];
     }
 
     public ArrayList<GraphNodeNeighbourList> getAdjacencyList() {
         return adjacencyList;
     }
+
+
 
     public void addUndirectedEdge(int i, int j){
        GraphNodeNeighbourList start = adjacencyList.get(i);
@@ -35,5 +46,42 @@ private ArrayList<GraphNodeNeighbourList> adjacencyList;
             s.append("\n");
         }
         return s.toString();
+    }
+
+    public void BreadthFirstSearchStart(int i){
+        GraphNodeNeighbourList u = adjacencyList.get(i);
+
+        for (int j = 0; j < adjacencyList.size(); j++) {
+            ante[i] = -1;
+            d[i]=-1;
+            cor[i] = 0;
+        }
+        BreadthFirstSearch(u);
+    }
+
+    private void BreadthFirstSearch(GraphNodeNeighbourList s){
+        Queue<GraphNodeNeighbourList> fila = new LinkedList<>();
+        fila.offer(s);
+        s.setStatus(GraphNodeNeighbourList.CINZA);
+        cor[adjacencyList.indexOf(s)] = GraphNodeNeighbourList.CINZA;
+        d[adjacencyList.indexOf(s)] = 0;
+
+        while(!fila.isEmpty()){
+            GraphNodeNeighbourList u = fila.poll();
+            int currentPos = adjacencyList.indexOf(u);
+            for (int i = 0; i <u.getNeighbours().size() ; i++) {
+                GraphNodeNeighbourList v= u.getNeighbours().get(i);
+                if(v.getStatus()==GraphNodeNeighbourList.BRANCO){
+                    int currentAdj = adjacencyList.indexOf(v);
+                    v.setStatus(GraphNodeNeighbourList.CINZA);
+                    cor[currentAdj] = GraphNodeNeighbourList.CINZA;
+                    d[currentAdj]=d[currentPos]+1;
+                    ante[currentAdj] = currentPos;
+                    fila.offer(v);
+            }
+            }
+            u.setStatus(GraphNodeNeighbourList.PRETO);
+            cor[currentPos] = GraphNodeNeighbourList.PRETO;
+        }
     }
 }
