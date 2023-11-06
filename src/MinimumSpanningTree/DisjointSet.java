d7cfd6c6a2e5396e62bd6b49d44d332bd2d2f643
GIL32610 <gil.00000032610@unicap.br>
2023-11-06 16:28:28 -0300

    Make Set and Find Set Methods

--- src/MinimumSpanningTree/DisjointSet.java
+++ src/MinimumSpanningTree/DisjointSet.java
@@ -1,4 +1,26 @@
 package MinimumSpanningTree;
 
+import java.util.ArrayList;
+
 public class DisjointSet {
+
+    private ArrayList< WeightedNode> nodeList;
+    private DisjointSet(){
+        this.nodeList= new ArrayList<>();
+    }
+
+    public static void makeSet(ArrayList<WeightedNode> nodeList){
+        for (int i = 0; i < nodeList.size(); i++) {
+            WeightedNode weightedNode = nodeList.get(i);
+            DisjointSet set = new DisjointSet();
+            set.nodeList.add(weightedNode);
+            weightedNode.setSet(set);
+        }
+    }
+
+    public static DisjointSet findSet (WeightedNode weightedNode){
+           DisjointSet set = weightedNode.getSet();
+           return set;
+    }
+
 }
--- src/MinimumSpanningTree/WeightedNode.java
+++ src/MinimumSpanningTree/WeightedNode.java
@@ -18,4 +18,52 @@ public class WeightedNode {
         this.name = name;
         this.distance = Integer.MAX_VALUE;
     }
+
+    public char getName() {
+        return name;
+    }
+
+    public ArrayList<WeightedNode> getNeighbours() {
+        return neighbours;
+    }
+
+    public HashMap<WeightedNode, Integer> getWeightMap() {
+        return weightMap;
+    }
+
+    public WeightedNode getParent() {
+        return parent;
+    }
+
+    public DisjointSet getSet() {
+        return set;
+    }
+
+    public int getDistance() {
+        return distance;
+    }
+
+    public void setName(char name) {
+        this.name = name;
+    }
+
+    public void setNeighbours(ArrayList<WeightedNode> neighbours) {
+        this.neighbours = neighbours;
+    }
+
+    public void setWeightMap(HashMap<WeightedNode, Integer> weightMap) {
+        this.weightMap = weightMap;
+    }
+
+    public void setParent(WeightedNode parent) {
+        this.parent = parent;
+    }
+
+    public void setSet(DisjointSet set) {
+        this.set = set;
+    }
+
+    public void setDistance(int distance) {
+        this.distance = distance;
+    }
 }
